32544dee1bdc147fba3dd6322ab47318c0d5fbd2
Rishav Raj <iamrishavraj1@gmail.com>
2021-02-28 23:00:52 +0530

    OOP Method

---
+++ python/day16/main.py
@@ -0,0 +1,38 @@
+from turtle import Turtle, Screen
+from prettytable import PrettyTable
+
+# turtle Module
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("red")
+# timmy.forward(100)
+# timmy.left(90)
+# timmy.forward(100)
+# timmy.right(90)
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+
+# preetytable package
+
+table = PrettyTable()
+
+table.add_column("Pokemon Name",
+                 [
+                     "Pikachu",
+                     "Squirtle",
+                     "Charmander"
+                  ])
+
+table.add_column("Type",
+                 [
+                     "Electric",
+                     "Water",
+                     "Fire"
+                  ])
+
+table.align = "l"
+print(table)
