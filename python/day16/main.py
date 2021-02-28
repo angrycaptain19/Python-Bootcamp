b115d65a2e6cfed616689135532d52af39f90784
Rishav Raj <iamrishavraj1@gmail.com>
2021-03-01 02:00:42 +0530

    turtle pratice

--- python/day16/main.py
+++ python/day16/main.py
@@ -2,37 +2,42 @@ from turtle import Turtle, Screen
 from prettytable import PrettyTable
 
 # turtle Module
-# timmy = Turtle()
-# print(timmy)
-# timmy.shape("turtle")
-# timmy.color("red")
-# timmy.forward(100)
-# timmy.left(90)
-# timmy.forward(100)
-# timmy.right(90)
-#
-# my_screen = Screen()
-# print(my_screen.canvheight)
-# my_screen.exitonclick()
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("red")
+timmy.forward(100)
+timmy.left(90)
+timmy.forward(100)
+timmy.right(270)
+timmy.forward(100)
+timmy.forward(100)
+timmy.left(90)
+timmy.forward(100)
+timmy.left(90)
+timmy.forward(100)
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
 
 
 # preetytable package
 
-table = PrettyTable()
-
-table.add_column("Pokemon Name",
-                 [
-                     "Pikachu",
-                     "Squirtle",
-                     "Charmander"
-                  ])
-
-table.add_column("Type",
-                 [
-                     "Electric",
-                     "Water",
-                     "Fire"
-                  ])
-
-table.align = "l"
-print(table)
+# table = PrettyTable()
+#
+# table.add_column("Pokemon Name",
+#                  [
+#                      "Pikachu",
+#                      "Squirtle",
+#                      "Charmander"
+#                   ])
+#
+# table.add_column("Type",
+#                  [
+#                      "Electric",
+#                      "Water",
+#                      "Fire"
+#                   ])
+#
+# table.align = "l"
+# print(table)
